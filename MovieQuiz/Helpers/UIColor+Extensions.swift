c568be05e4ea575db4d1a0fba455f2f8f7f6dbf5
Anton Zverkov <walltouch@gmail.com>
2025-02-22 17:06:06 +0300

    commit before refactoring

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+//По аналогии создайте статические переменные для оставшихся цветов, которые есть в макетах. Финальный вариант файла UIColor+Extensions.swift должен выглядеть так:
+//     static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+//     static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+//     static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+//     static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+//     static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+//     static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+// WTF Practicum?
+}
